ed4f3dd2556dfb5c576458ea38b4dbdb5f74df40
Sajidh Yazeen <155213605+Agentz47@users.noreply.github.com>
2025-02-02 17:28:07 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import ProductCard from './components/productCard'
+function App() {
+
+
+  return (
+    <div>
+      <ProductCard name ="Watch" price ="500/-" 
+      
+      description ="dsadsadsasdvvvbbnn" 
+      
+      photoUrl ="https://img.drz.lazcdn.com/static/lk/p/4969c39e68a7cbd19d468af022314064.jpg_400x400q75.jpg_.webp" />
+
+      <ProductCard name="sdadsa" price="dsdsd" photoUrl="https://img.drz.lazcdn.com/g/kf/Se428261dc3f14d92b082f0aed2f6a4baB.png_400x400q75.png_.webp" />
+
+    </div>
+    
+  )
+}
+
+export default App
